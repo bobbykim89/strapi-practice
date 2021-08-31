dd16030ddcd78c015c49670b855adf1d9b42c450
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-08-30 18:19:44 -0700

    fetch data using graphql and added some new features

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      title
+      body
+      rating
+      id
+      categories {
+        name
+        id
+      }
+    }
+  }
+`;
 
 const Homepage = () => {
-  const { loading, error, data } = useFetch('http://localhost:1337/reviews');
+  const { loading, error, data } = useQuery(REVIEWS);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -13,12 +28,14 @@ const Homepage = () => {
   }
   return (
     <div>
-      {data &&
-        data.map((review) => (
+      {data.reviews &&
+        data.reviews.map((review) => (
           <div key={review.id} className='review-card'>
             <div className='rating'>{review.rating}</div>
             <h2>{review.title}</h2>
-            <small>console list</small>
+            {review.categories.map((c) => (
+              <small key={c.id}>{c.name}</small>
+            ))}
             <p>{review.body.substring(0, 200)}...</p>
             <Link to={`/details/${review.id}`}>Read More</Link>
           </div>
--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import useFetch from '../hooks/useFetch';
+import { useQuery, gql } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      title
+      body
+      rating
+      id
+      categories {
+        id
+        name
+      }
+    }
+  }
+`;
 
 const ReviewDetails = () => {
   const { id } = useParams();
-  const { loading, error, data } = useFetch(
-    'http://localhost:1337/reviews/' + id
-  );
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id: id },
+  });
 
   if (loading) return <p>Loading...</p>;
 
@@ -16,10 +32,12 @@ const ReviewDetails = () => {
     <div>
       {data ? (
         <div className='review-card'>
-          <div className='rating'>{data.rating}</div>
-          <h2>{data.title}</h2>
-          <small>console list</small>
-          <p>{data.body}</p>
+          <div className='rating'>{data.review.rating}</div>
+          <h2>{data.review.title}</h2>
+          {data.review.categories.map((c) => (
+            <small key={c.id}>{c.name}</small>
+          ))}
+          <ReactMarkdown>{data.review.body}</ReactMarkdown>
         </div>
       ) : (
         ''
